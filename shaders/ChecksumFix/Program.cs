eb4a552c1d2acc817b62dc3e4dace1a268ecafc2
Zorrot Chen <UI_Zorrot@163.com>
2021-06-24 15:56:33 +0800

    add shaders

---
+++ shaders/ChecksumFix/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChecksumFix
+{
+    class Program
+    {
+        // For testing / disassembling hand-edited shaders
+
+        static void Main(string[] args)
+        {
+            byte[] shader = File.ReadAllBytes(args[0]);
+            int[] checksum = DXBCChecksum.DXBCChecksum.CalculateDXBCChecksum(shader);
+            byte[] bChecksum = new byte[16];
+            Buffer.BlockCopy(checksum, 0, bChecksum, 0, 16);
+            bChecksum.CopyTo(shader, 4);
+            File.WriteAllBytes(args[0], shader);
+        }
+    }
+}
